7ba5e3e91a89ed51074483562c2f78c253ff2533
Nikki <nikki@launchpadlab.com>
2023-11-29 09:40:40 -0500

    update with full relations addition

--- admin.js
+++ admin.js
@@ -1,7 +1,6 @@
+/* eslint-disable import/no-unresolved*/
 import prisma from '../lib/prisma.js'
-/* eslint-disable-next-line import/no-unresolved*/
 import { getModelByName } from '@adminjs/prisma'
-/* eslint-disable-next-line import/no-unresolved*/
 import { ComponentLoader } from 'adminjs'
 import {
   targetRelationSettingsFeature,
@@ -9,33 +8,21 @@ import {
 } from '@adminjs/relations'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import camelCase from 'lodash/camelCase.js'
+import lowerCase from 'lodash/lowerCase.js'
+import { MODELS } from '../../src/utils/constants.js'
+import resourceOptions from './utils/resourceOptions.js'
+import {
+  serializeFileResponse,
+  serializeFilesResponse,
+} from './utils/serializers.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const MODELS = {
-  USER: 'User',
-  FILE: 'File',
-  FOLDER: 'Folder',
-  AUTHORIZATION: 'Authorization',
-}
-
 const RELATIONSHIP_TYPE = {
   ONE_TO_MANY: 'one-to-many',
   MANY_TO_MANY: 'many-to-many',
-}
-
-const DEFAULT_RESOURCE_OPTIONS = {
-  properties: {
-    id: {
-      isVisible: {
-        edit: false,
-        show: false,
-        list: false,
-        filter: true,
-      },
-    },
-  },
 }
 
 export class Admin {
@@ -46,6 +33,14 @@ export class Admin {
         'CustomDashboard',
         path.resolve(__dirname, 'views/Dashboard')
       ),
+      UserName: this.componentLoader.add(
+        'UserName',
+        path.resolve(__dirname, 'components/UserName')
+      ),
+      AuthorizationForm: this.componentLoader.add(
+        'AuthorizationForm',
+        path.resolve(__dirname, 'forms/AuthorizationForm')
+      ),
     }
   }
 
@@ -55,7 +50,7 @@ export class Admin {
         model: getModelByName(model),
         client: prisma,
       },
-      options: DEFAULT_RESOURCE_OPTIONS,
+      options: resourceOptions.defaultProperties,
     }
   }
 
@@ -63,13 +58,8 @@ export class Admin {
     return [
       this.createUserResource(),
       this.createFolderResource(),
+      this.createFileResource(),
       this.createAuthorizationResource(),
-      this.createDefaultResource('File'),
-      this.createDefaultResource('Case'),
-      this.createDefaultResource('Proceeding'),
-      this.createDefaultResource('UserProceeding'),
-      this.createDefaultResource('LoginLink'),
-      this.createDefaultResource('SignIn'),
     ]
   }
 
@@ -83,55 +73,157 @@ export class Admin {
     }
   }
 
+  createFileResource = () => {
+    const defaults = this.createDefaultResource(MODELS.FILE)
+
+    return {
+      ...defaults,
+      options: {
+        ...defaults.options,
+        actions: {
+          ...defaults.options.actions,
+          list: {
+            after: serializeFilesResponse,
+          },
+          show: {
+            after: serializeFileResponse,
+          },
+        },
+        properties: {
+          ...defaults.options.properties,
+          user: {
+            ...resourceOptions.neverVisible,
+          },
+        },
+      },
+      features: [
+        this.createOwningRelationSettingsFeature(MODELS.FILE, [
+          {
+            modelName: MODELS.AUTHORIZATION,
+            relationshipType: RELATIONSHIP_TYPE.ONE_TO_MANY,
+          },
+        ]),
+      ],
+    }
+  }
+
   createFolderResource = () => {
     return {
-      ...this.createDefaultResource('Folder'),
-      features: [
-        owningRelationSettingsFeature({
-          componentLoader: this.componentLoader,
-          licenseKey: process.env.ADMINJS_RELATIONS_KEY,
-          relations: {
-            authorizations: {
-              type: RELATIONSHIP_TYPE.ONE_TO_MANY,
-              target: {
-                joinKey: 'folderId',
-                resourceId: 'Authorization',
-              },
-            },
+      ...this.createDefaultResource(MODELS.FOLDER),
+      options: {
+        properties: {
+          id: {
+            ...resourceOptions.neverVisible,
           },
-        }),
+          user: {
+            ...resourceOptions.neverVisible,
+          },
+        },
+      },
+      features: [
+        this.createOwningRelationSettingsFeature(MODELS.FOLDER, [
+          {
+            modelName: MODELS.AUTHORIZATION,
+            relationshipType: RELATIONSHIP_TYPE.ONE_TO_MANY,
+          },
+          {
+            modelName: MODELS.FILE,
+            relationshipType: RELATIONSHIP_TYPE.ONE_TO_MANY,
+          },
+        ]),
       ],
     }
   }
 
   createUserResource = () => {
     return {
-      ...this.createDefaultResource('User'),
+      ...this.createDefaultResource(MODELS.USER),
       features: [
-        owningRelationSettingsFeature({
-          componentLoader: this.componentLoader,
-          licenseKey: process.env.ADMINJS_RELATIONS_KEY,
-          relations: {
-            authorizations: {
-              type: RELATIONSHIP_TYPE.ONE_TO_MANY,
-              target: {
-                joinKey: 'userId',
-                resourceId: 'Authorization',
-              },
-            },
-          }
-        })
-      ]
+        this.createOwningRelationSettingsFeature(MODELS.USER, [
+          {
+            modelName: MODELS.AUTHORIZATION,
+            relationshipType: RELATIONSHIP_TYPE.ONE_TO_MANY,
+          },
+          {
+            modelName: MODELS.SIGN_IN,
+            relationshipType: RELATIONSHIP_TYPE.ONE_TO_MANY,
+          },
+        ]),
+      ],
     }
   }
 
   createAuthorizationResource = () => {
     return {
-      resource: {
-        model: getModelByName('Authorization'),
-        client: prisma,
+      ...this.createDefaultResource(MODELS.AUTHORIZATION),
+      ...this.createTargetResourceFeature(),
+      options: {
+        properties: {
+          id: {
+            isVisible: {
+              list: false,
+              show: true,
+              edit: false,
+              filter: true,
+            },
+          },
+          user: {
+            ...resourceOptions.neverVisible,
+          },
+        },
+        actions: {
+          new: {
+            component: this.components.AuthorizationForm,
+          },
+        },
       },
+    }
+  }
+
+  createSignInResource = () => {
+    const defaults = this.createDefaultResource(MODELS.SIGN_IN)
+
+    return {
+      ...defaults,
+      options: {
+        ...defaults.options,
+        actions: {
+          new: {
+            isAccessible: false,
+          },
+        },
+      },
+      ...this.createTargetResourceFeature(),
+    }
+  }
+
+  createTargetResourceFeature = () => {
+    return {
       features: [targetRelationSettingsFeature()],
     }
   }
+
+  // targetModels is array of objects in format { modelName: 'User', relationshipType: 'one-to-many' }
+  createOwningRelationSettingsFeature = (parentModel, targetModels) => {
+    let relationsObj = {}
+    targetModels.forEach(({ modelName, relationshipType }) => {
+      const key = camelCase(modelName) + 's'
+      relationsObj[key] = {
+        type: relationshipType,
+        target: {
+          joinKey: lowerCase(parentModel),
+          resourceId: modelName,
+        },
+      }
+    })
+
+    return owningRelationSettingsFeature({
+      componentLoader: this.componentLoader,
+      licenseKey: process.env.ADMINJS_RELATIONS_KEY,
+      relations: {
+        ...relationsObj,
+      },
+    })
+  }
+
 }
